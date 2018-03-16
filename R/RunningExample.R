5570ff2147950b1915b6c6544f656cccec5a5ccc
KarimOualkacha <oualkacha.karim@uqam.ca>
2018-03-16 09:50:45 -0400

    Update RunningExample.R

--- R/RunningExample.R
+++ R/RunningExample.R
@@ -14,12 +14,12 @@ library(matrixcalc)
 library(rARPACK)
 #----------------------------------------------#
 #----------------------------------------------#
-setwd("/Users/KOualkachaUQAM/Dropbox/Etudiants/Jianping/SSCcode/Codes4JS/CBMRV/R")
+#setwd("/Users/KOualkachaUQAM/Dropbox/Etudiants/Jianping/SSCcode/Codes4JS/CBMRV/R")
 source("functions.R")
 source("inputChecksCBMRV.R")
 source("mainCBM-RV.R")
 source("CBM-RV-Null.R")
-setwd("/Users/KOualkachaUQAM/Dropbox/Etudiants/Jianping/SSCcode/Codes4JS/CBMRV/data")
+#setwd("/Users/KOualkachaUQAM/Dropbox/Etudiants/Jianping/SSCcode/Codes4JS/CBMRV/data")
 load("kin1.Rdata")
 kin <- 2*kin1
 Geno = as.matrix(read.table("genotypes.dat",sep = " "))
